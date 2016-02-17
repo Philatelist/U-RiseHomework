281a71664b5a4ec5b4db971bd339d20d4813469f
Alexander Usov <a.usoff@gmail.com>
2016-02-17 02:02:42 +0200

    Create two text field and button. The button can change second field after one click.

--- app/src/main/java/com/slavyanin/example/u_risehomework/MainActivity.java
+++ app/src/main/java/com/slavyanin/example/u_risehomework/MainActivity.java
@@ -2,12 +2,25 @@ package com.slavyanin.example.u_risehomework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView countText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countText = (TextView) findViewById(R.id.textCount);
+
+    }
+
+    public void onClick(View v) {
+        countText.setText("Count");
     }
 }
