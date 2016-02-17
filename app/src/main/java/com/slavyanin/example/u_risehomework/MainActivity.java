647c5a17f701e0c890d9b3973ae20a3da66dca03
Alexander Usov <a.usoff@gmail.com>
2016-02-17 02:19:17 +0200

    The button can change second field after each clicks (count +1).

--- app/src/main/java/com/slavyanin/example/u_risehomework/MainActivity.java
+++ app/src/main/java/com/slavyanin/example/u_risehomework/MainActivity.java
@@ -3,6 +3,7 @@ package com.slavyanin.example.u_risehomework;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -10,17 +11,22 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView countText;
+    private Button countButton;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        countButton = (Button) findViewById(R.id.button);
         countText = (TextView) findViewById(R.id.textCount);
 
-    }
-
-    public void onClick(View v) {
-        countText.setText("Count");
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countText.setText("Count - " + ++count);
+            }
+        });
     }
 }
